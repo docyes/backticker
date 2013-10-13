b6106c866d368bd8a202eece9580dde8a0e0adc0
docyes <carl@featureblend.com>
2013-10-12 22:25:06 -0700

    complete param testing for constructor

--- test/index.js
+++ test/index.js
@@ -42,11 +42,27 @@
         this.clock.tick(interval);
         equal(this.callback.callCount, 1, 'tick emitted after default interval');
     });
+    test('initialize with single param', 2, function() {
+        var ticker = new Backbone.Ticker({params: 'hello'});
+        ticker.on('tick', this.callback);
+        this.clock.tick(1000-1);
+        equal(this.callback.callCount, 0, 'tick not emitted before default interval');
+        this.clock.tick(1000);
+        equal(this.callback.callCount, 0, 'tick not emitted after default interval because never started');
+    });
     test('initialize with single param start true', 1, function() {
         var ticker = new Backbone.Ticker({start: true, params: 'hello'});
         ticker.on('tick', this.callback);
         this.clock.tick(1000);
         ok(this.callback.calledWith('hello'), 'tick called with single argument');    
+    });
+    test('initialize with multi-param', 2, function() {
+        var ticker = new Backbone.Ticker({params: ['hello', 'world']});
+        ticker.on('tick', this.callback);
+        this.clock.tick(1000-1);
+        equal(this.callback.callCount, 0, 'tick not emitted before default interval');
+        this.clock.tick(1000);
+        equal(this.callback.callCount, 0, 'tick not emitted after default interval because never started');
     });
     test('initialize with multi-param start true', 1, function() {
         var ticker = new Backbone.Ticker({start: true, params: ['hello', 'world']});
